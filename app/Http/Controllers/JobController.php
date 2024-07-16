f7f62f27abcb1a357a79ce98bde4c7ed39cfdeaf
George Higbie (Spectre) <gehigbie@gmail.com>
2024-07-16 18:48:13 -0500

    Add some basic auth and update model relationships

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class JobController extends Controller
 {
@@ -43,6 +44,14 @@ class JobController extends Controller
 
 
     public function edit(Job $job){
+        if(Auth::guest()){
+            return redirect('/jobs');
+        }
+
+        if($job->employer->user->isNot(Auth::user())){
+            abort(403);
+        }
+
         return view('jobs.edit', ['job'=> $job]);
     }
 
--- database/migrations/2024_07_07_191346_create_employers_table.php
+++ database/migrations/2024_07_07_191346_create_employers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('location');
+            $table->foreignIdFor(\App\Models\User::class);
             $table->timestamps();
         });
     }
